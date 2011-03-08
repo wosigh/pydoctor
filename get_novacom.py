162ffd7eda9eca98ab6f03ec25e423694817b7ee
Ryan Hope <rmh3093@gmail.com>
2011-03-08 12:33:53 -0500

    first cut, reqires platinfo module

---
+++ get_novacom.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import argparse
+from platinfo import PlatInfo
+from httpunzip import *
+
+jars = ['http://palm.cdnetworks.net/rom/pre2/p201r0d11242010/wrep201rod/webosdoctorp102ueuna-wr.jar']
+
+NOVA_WIN32  = 'resources/NovacomInstaller_x86.msi'
+NOVA_WIN64  = 'resources/NovacomInstaller_x64.msi'
+NOVA_MACOSX = 'resources/NovacomInstaller.pkg.tar.gz'
+
+def download_novacom_installer(os, url, path):
+    if os == 'win32':
+        http_unzip(url, NOVA_WIN32, path)
+    elif os == 'win64':
+        http_unzip(url, NOVA_WIN64, path)
+    elif os == 'macosx':
+        http_unzip(url, NOVA_MACOSX, path)
+
+if __name__ == '__main__':
+    
+    pi = PlatInfo()
+
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,description=jars)
+    parser.add_argument('-l', '--list', action="store_true", dest="list", help='List known jar urls.')
+    parser.add_argument('-p', '--path', action="store", dest="targetpath", help='Target path for extracted files.')
+    group = parser.add_argument_group('required arguments')
+    group.add_argument('-u', '--url', action="store", dest="url", help='The URL of the target zip or jar file.', metavar='URL')
+    args = parser.parse_args()
+    
+    if args.list:
+        for jar in jars:
+            print jar
+    elif args.url: 
+        download_novacom_installer(pi.os, args.url, args.targetpath)
