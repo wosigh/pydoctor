1d46cfd2705dd30091b16f6c8615040dbdbacad8
Ryan Hope <rmh3093@gmail.com>
2011-03-14 00:52:56 -0400

    fix large file support

--- novacom2.py
+++ novacom2.py
@@ -24,18 +24,22 @@ class Novacom(Protocol):
                 if not self.header and len(self.buffer) > 15:
                     self.header = struct.unpack('<IIII', self.buffer[0:16])
                     self.buffer = self.buffer[16:]
-                if self.header and self.header[3] == 0:
-                    self.stdout = ''.join([self.stdout, self.buffer[:self.header[2]]])
-                    self.buffer = self.buffer[self.header[2]:]
-                    self.header = None
-                if self.header and self.header[3] == 1:
-                    self.stderr = ''.join([self.stderr, self.buffer[:self.header[2]]])
-                    self.buffer = self.buffer[self.header[2]:]
-                    self.header = None
-                if self.header and self.header[3] == 2:
-                    self.oob = struct.unpack('<IIIII', self.buffer[:self.header[2]])
-                    self.buffer = self.buffer[self.header[2]:]
-                    self.header = None
+                if self.header: 
+                    if len(self.buffer) >= self.header[2]:
+                        if self.header[3] == 0:
+                            self.stdout = ''.join([self.stdout, self.buffer[:self.header[2]]])
+                            self.buffer = self.buffer[self.header[2]:]
+                            self.header = None
+                        elif self.header[3] == 1:
+                            self.stderr = ''.join([self.stderr, self.buffer[:self.header[2]]])
+                            self.buffer = self.buffer[self.header[2]:]
+                            self.header = None
+                        elif self.header[3] == 2:
+                            self.oob = struct.unpack('<IIIII', self.buffer[:self.header[2]])
+                            self.buffer = self.buffer[self.header[2]:]
+                            self.header = None
+                    else:
+                        break
                 if self.oob and self.oob[0] == 0:
                     if self.oob[1] == 1:
                         self.cmd_stdout(self.stdout)
@@ -46,10 +50,11 @@ class Novacom(Protocol):
                     self.cmd_return(self.oob[1])
                     self._reset()
             else:
-                msg = self.buffer.split('\n')[0]
+                i = self.buffer.find('\n')
+                msg = self.buffer[:i]
                 if msg == 'ok 0':
                     self.status = True
-                    self.buffer = ''
+                    self.buffer = self.buffer[i+1:]
                 else:
                     self.error(msg)
                     self._reset()
@@ -88,12 +93,3 @@ class DeviceCollector(Protocol):
         for d in data[:-1].split('\n'):
             d = d.split(' ')
             self.devices.append((int(d[0]), d[1], d[2], d[3]))
-
-if __name__ == '__main__':
-    factory = Factory()
-    factory.devices = []
-    factory.protocol = DeviceCollector
-    point = TCP4ClientEndpoint(reactor, 'localhost', 6968)
-    d = point.connect(factory)
-    reactor.run()
-    
