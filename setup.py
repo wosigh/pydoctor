bda66d28e04e95dd104fc03d0209c591117806a1
Ryan Hope <rmh3093@gmail.com>
2011-03-17 15:29:23 -0400

    add novatool setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+import py2exe
+
+py2exe_options = dict(
+    ascii = True,
+    excludes = ['_ssl','pyexpat','bz2'],
+    bundle_files = True,
+    compressed = True
+)
+
+setup(
+    windows = [
+        {
+            'script': 'novatool.py',
+            'icon_resources': [(1, 'novacomInstaller.ico')]
+        }
+    ],
+    options = {'py2exe': py2exe_options},
+    zipfile = None,
+    description = 'Cross-platform novacom tool',
+    author = 'Webos-Internals',
+    maintainer = 'Ryan Hope',
+    maintainer_email = 'rmh3093@gmail.com',
+    version = '1.0',
+    name = 'Novatool'
+)
